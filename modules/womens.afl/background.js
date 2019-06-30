0525b3c8758beb92b222ba508e68711ea880386f
Tommo Crabb <33530217+TommoCrabb@users.noreply.github.com>
2019-07-01 04:17:57 +1000

    Getting everything synced to Github

--- modules/womens.afl/background.js
+++ modules/womens.afl/background.js
@@ -17,31 +17,35 @@ aflw.init = function(input) {
 	});
 }
 
-aflw.requestListener = function() {
-	browser.webRequest.onBeforeRequest.addListener(
-		aflw.playlistener,
-		{urls: [
-			"https://bpmultihlsvods257.ngcdn.telstra.com/hls-vod/ingest_aflfilms_vod/*/VOD_Source/*/output_*kbps_*p.mp4.m3u8*"
-		]}
-	);
-}
-
-aflw.playlistener = function(input) {
-	console.log(">>> aflw.playlistener fired in tab #" + input.tabId);
-	aflw.playlist = input.url.replace(
-			/^(https?:\/\/.*\/output)_\d+kbps_\d+p(\.mp4\.m3u8).*/,
-		"$1_2400kbps_720p$2"
-	);
-	console.log(">>> " + aflw.playlist);
-	browser.pageAction.show(input.tabId);
-	browser.pageAction.setPopup({
-		tabId : input.tabId,
-		popup : "/modules/womens.afl/popup.html"
-	});
-	browser.webRequest.onBeforeRequest.removeListener(aflw.playlistener);
-}
-
 browser.webNavigation.onDOMContentLoaded.addListener(
 	aflw.init,
 	aflw.filter
 );
+
+
+
+// aflw.requestListener = function() {
+// 	browser.webRequest.onBeforeRequest.addListener(
+// 		aflw.playlistener,
+// 		{urls: [
+// 			"https://bpmultihlsvods257.ngcdn.telstra.com/hls-vod/ingest_aflfilms_vod/*/VOD_Source/*/output_*kbps_*p.mp4.m3u8*"
+// 		]}
+// 	);
+// }
+
+// aflw.playlistener = function(input) {
+// 	console.log(">>> aflw.playlistener fired in tab #" + input.tabId);
+// 	aflw.playlist = input.url.replace(
+// 			/^(https?:\/\/.*\/output)_\d+kbps_\d+p(\.mp4\.m3u8).*/,
+// 		"$1_2400kbps_720p$2"
+// 	);
+// 	console.log(">>> " + aflw.playlist);
+// 	browser.pageAction.show(input.tabId);
+// 	browser.pageAction.setPopup({
+// 		tabId : input.tabId,
+// 		popup : "/modules/womens.afl/popup.html"
+// 	});
+// 	browser.webRequest.onBeforeRequest.removeListener(aflw.playlistener);
+// }
+
+//
--- modules/womens.afl/content.js
+++ modules/womens.afl/content.js
@@ -1,13 +1,6 @@
 console.log(">>> LOADED: /modules/womens.afl/content.js");
+
 /*
-var click = new Event("click");
-var vids = document.querySelectorAll('article.js-video-list-item');
-for (let v of vids) {
-	v.classList.remove("is-playing");
-}
-
-
-
 ///Test Injection///
 
 var s = document.createElement("script");
@@ -19,49 +12,64 @@ document.body.appendChild(s);
 
 ///End Test///
 */
-var click = new Event("click");
-var vids = document.querySelectorAll('article.js-video-list-item');
+
+// COMMENT: Build an object "data" that will be sent to the popup script
+
 var meta = document.querySelectorAll('script[data-match-structure]');
 meta = JSON.parse(meta[0].text);
 
+var data = {
+	message : "vid info",
+	meta : meta,
+	vids : { }
+};
+
+var articles = document.querySelectorAll('article.js-video-list-item');
+for (let a of articles) {
+	let v = {
+		done : false,
+		tries : 0
+	};
+	v.duration = a.querySelectorAll('span.video-duration__duration')[0].innerHTML;
+	v.title = a.getAttribute('data-title-override');
+	v.id = a.getAttribute('data-embed-code');
+	data.vids[v.id] = v;
+}
+
+// COMMENT: Set up a messaging port to popup script
+
 var bgPort = browser.runtime.connect({name: "womens.afl content"});
 bgPort.onMessage.addListener( function(m) {
+	console.log(">>> RECEIVED MESSAGE: " + m.message);
 	switch (m.message) {
 	case "go":
-		go(vids);
+		console.log(">>> SENDING DATA FROM CONTENT.JS TO POPUP.JS");
+		console.log(data);
+		bgPort.postMessage(data);
 		break;
 	case "play":
+		console.log(">>> CONTENT.JS RECEIVED DATA FROM POPUP.JS");
 		play(m.id);
 		break;
-	case "send skipped":
-		
 	}
 });
 
-function go(vids) {
-	var items = [];
-	for (let v of vids) {
-		items.push({
-			duration : v.querySelectorAll('span.video-duration__duration')[0].innerHTML,
-			title : v.getAttribute('data-title-override')
-		});
-	}
-	bgPort.postMessage({
-		message: "vid info",
-		items: items,
-		meta: meta
-	});
-}
+// COMMENT: Play video when asked by popup
+
+var click = new Event("click");
 
 function play(id){
-	console.log(">>> PLAY ID: " + id)
-	if ( vids[id].classList.contains("is-playing") ) {
+	let v = data.vids[id];
+	let e = articles.querySelectorAll('[data-embed-code=' + id + ']')[0];
+	if ( e.classList.contains("is-playing") ) {
+		console.log(">>> SKIPPING VIDEO WITH ID: " + id)
 		bgPort.postMessage({
 			message : "skipped",
 			id : id
 		})
 	} else {
-		vids[id].dispatchEvent(click);
+		console.log(">>> PLAYING VIDEO WITH ID: " + id)
+		e.dispatchEvent(click);
 		bgPort.postMessage({
 			message : "started playing",
 			id : id
